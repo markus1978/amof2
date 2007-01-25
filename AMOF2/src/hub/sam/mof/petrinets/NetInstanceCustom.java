6d48b3450802b04d16f4353249fbf03f848b9c38
Markus Scheidgen <do@not.use>
2007-01-25 20:04:15 +0000

    new mof action semantics based on petri nets and the graphical mas representation. bugfixes.

---
+++ AMOF2/src/hub/sam/mof/petrinets/NetInstanceCustom.java
@@ -0,0 +1,29 @@
+package hub.sam.mof.petrinets;
+
+import hub.sam.mof.mas.ActivityNode;
+import hub.sam.mof.util.ListImpl;
+import cmof.common.ReflectiveCollection;
+
+public class NetInstanceCustom extends NetInstanceDlg {
+
+	@Override
+	public void run() {		
+		
+		ReflectiveCollection<? extends Transition> activeTransitions = self.getEnabledTransitions();		
+		while(activeTransitions.size() > 0) {			
+			activeTransitions.iterator().next().fire(self);			
+			activeTransitions = self.getEnabledTransitions();
+		}
+	}
+	
+	@Override
+	public ReflectiveCollection<? extends Transition> getEnabledTransitions() {
+		ReflectiveCollection<Transition> result = new ListImpl<Transition>();
+		for (Transition transition: self.getMetaClassifierNet().getTransitions()) {
+			if (transition.isEnabled(self)) {
+				result.add(transition);
+			}
+		}
+		return result;
+	}
+}
